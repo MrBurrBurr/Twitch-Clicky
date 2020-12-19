d2e8923c6cea0ab39d083735b283dc4f6329039f
FREDERICK <github@FREDERICK.win>
2020-12-19 09:36:24 +0100

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch-Clicky
 // @namespace    https://frederick.win/
-// @version      0.2
+// @version      0.3
 // @description  Automatically opens the twitch gift box for you.
 // @author       FREDERICK
 // @match        https://www.twitch.tv/*

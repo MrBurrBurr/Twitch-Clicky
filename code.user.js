1986df073482ce77d66d23e2a7c2ac407196006b
FREDERICK <github@FREDERICK.win>
2021-05-18 01:05:15 +0200

    Misc

--- code.user.js
+++ code.user.js
@@ -14,13 +14,12 @@
   'use strict';
   
   function checkForPoints() {
-    //const elem = document.querySelectorAll('button.tw-button.tw-button--success');
     const potentials = document.querySelectorAll("[class*=ScCoreButtonSuccess]");
     
     elementLoop:
     for (let i = 0; i < potentials.length; i++) {
       let potential = potentials[i];
-      
+
       for (let j = 0; j < potential.classList.length; j++) {
         if (potential.classList[j].match(/^ScCoreButtonSuccess/)) {
           potential.click();

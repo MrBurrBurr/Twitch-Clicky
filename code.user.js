e411d80d520235eead1ed91a3b2b9757cee33344
FREDERICK <github@FREDERICK.win>
2021-09-02 10:55:35 +0200

    Misc

--- code.user.js
+++ code.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Twitch-Clicky
-// @namespace    https://frederick.win/
-// @version      0.5
+// @namespace    https://github.com/MrBurrBurr
+// @version      0.6
 // @description  Automatically opens the twitch gift box for you.
 // @author       FREDERICK
 // @match        https://www.twitch.tv/*
+// @icon         https://www.google.com/s2/favicons?domain=twitch.tv
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/MrBurrBurr/Twitch-Clicky/master/code.user.js
-// @updateURL    https://raw.githubusercontent.com/MrBurrBurr/Twitch-Clicky/master/code.user.js
+// @downloadURL  https://github.com/MrBurrBurr/Twitch-Clicky/raw/master/code.user.js
+// @updateURL    https://github.com/MrBurrBurr/Twitch-Clicky/raw/master/code.user.js
 // ==/UserScript==
 
 (function() {
